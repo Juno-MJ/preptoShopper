f0b2ba0904e4a957d01e3f0591d3fd5958dd86da
Prakash <prakas.kanna.subramanian@gmail.com>
2018-05-30 23:56:40 +0530

    Static resources [bootstrap modules] setup done

--- BuyMe/src/main/java/juno/BuyMe/controller/pageController.java
+++ BuyMe/src/main/java/juno/BuyMe/controller/pageController.java
@@ -1,7 +1,9 @@
 package juno.BuyMe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +13,26 @@ public class pageController {
 	public ModelAndView hitterPage() {
 		ModelAndView modelAndView = new ModelAndView("page");
 		modelAndView.addObject("hitMessage","Hit Done");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/testingRP")
+	public ModelAndView requestParamDemo(@RequestParam(value="greeting",required=false) String greetMsg) {
+		if(greetMsg==null) {
+			greetMsg="This is a default msg";
+		}
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("hitMessage",greetMsg);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/testingPathVar/{greeting}")
+	public ModelAndView pathVariableDemo(@PathVariable(value="greeting",required=false) String greetMsg) {
+		if(greetMsg==null) {
+			greetMsg="This is a default msg";
+		}
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("hitMessage",greetMsg);
 		return modelAndView;
 	}
 	
